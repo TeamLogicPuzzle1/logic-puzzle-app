33347ca1dd7c5c8486928bb5646cf6f9f4a7f28f
jin <hsh410711@naver.com>
2024-09-22 18:19:59 +0900

    Fixing Code

--- Test_UI/app/src/main/java/com/cookandroid/test_ui/SettingAlarm.java
+++ Test_UI/app/src/main/java/com/cookandroid/test_ui/SettingAlarm.java
@@ -1,3 +1,9 @@
+/*
+ * 간략: 알람 설정 창
+ * 최초 작성자: 홍진기
+ * 작성일: 2024-09-21
+ * 버전: 0.0.1
+ * */
 package com.cookandroid.test_ui;
 
 import android.content.Intent;
@@ -10,6 +16,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingAlarm extends AppCompatActivity {
+    /*
+    * 변수명
+    * SettingBackBtn (환경 설정 이전으로 돌아가는 버튼)*/
     Intent intent;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
--- Test_UI/app/src/main/java/com/cookandroid/test_ui/SettingLeaderVer.java
+++ Test_UI/app/src/main/java/com/cookandroid/test_ui/SettingLeaderVer.java
@@ -1,3 +1,9 @@
+/*
+ * 간략: 리더 환경 설정 창
+ * 최초 작성자: 홍진기
+ * 작성일: 2024-09-22
+ * 버전: 0.0.1
+ * */
 package com.cookandroid.test_ui;
 
 import android.content.Intent;
@@ -13,6 +19,9 @@ public class SettingLeaderVer extends AppCompatActivity {
     Intent intent;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        /*
+        * 변수명
+        * alarmSettingBtn (알람 설정으로 들어가는 버튼)*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_leader_ver);
         AppCompatButton alarmSettingBtn;
