0d687efd3114d207becc3873e6015aa8a9dbc233
jin <hsh410711@naver.com>
2024-09-27 11:21:58 +0900

    Fixing code

--- Test_UI/app/src/main/java/com/cookandroid/test_ui/CreateAccount.java
+++ Test_UI/app/src/main/java/com/cookandroid/test_ui/CreateAccount.java
@@ -8,8 +8,10 @@ package com.cookandroid.test_ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,8 +20,10 @@ public class CreateAccount extends AppCompatActivity {
     /*
     * 변수명 returnSignInBtn(회원가입 버튼)
     */
-    Button careturnSignInBtn;
+    TextView certification_Timer;
+    Button careturnSignInBtn, certificationNumberSendBtn;
     Intent intent;
+    boolean isTimerRunning = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +36,35 @@ public class CreateAccount extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        /* certificationNumberSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isTimerRunning) {
+                    startTimer();
+                }
+            }
+        }); */
     }
+
+    /* protected void startTimer() {
+        CountDownTimer countDownTimer;
+        countDownTimer = new CountDownTimer(180000, 1000) {
+            @Override
+            public void onTick(long l) {
+                int minutes = (int) (l / 1000) / 60;
+                int seconds = (int) (l / 1000) % 60;
+                String timeFormatted = String.format("%02d:%02d", minutes , seconds);
+                certification_Timer.setText(timeFormatted);
+            }
+
+            @Override
+            public void onFinish() {
+                certification_Timer.setText("00:00");
+                 isTimerRunning = false;
+            }
+        };
+        countDownTimer.start();
+        isTimerRunning = true;
+        certificationNumberSendBtn.setEnabled(false);
+    } */
 }
